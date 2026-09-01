77c53bf79a89092aa5ded61062a11100dc2fb6be
LAB27_USER ON WS35 <L27Y15W35@LabHCI.net>
2026-09-01 16:55:01 +0800

    code

--- crms/config/settings.py
+++ crms/config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "students",
+    "classes",
 ]
 
 MIDDLEWARE = [
@@ -124,6 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT=BASE_DIR/"staticfiles"
+STATICFILES_DIRS=[BASE_DIR/"static"]
 
 
 # Email
--- crms/config/urls.py
+++ crms/config/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("students/", include("students.urls")),
-    path("", include("students.urls")),
+    path("classes/", include("classes.urls")),
+    path("", 
+         TemplateView.as_view(template_name="home/home.html"),
+         name="home",
+         ),
 ]
