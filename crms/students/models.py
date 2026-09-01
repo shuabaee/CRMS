57f08d7e2238dd419b82364d1bc7c9c0b07eec48
LAB27_USER ON WS35 <L27Y15W35@LabHCI.net>
2026-09-01 15:30:01 +0800

    initial commit

---
+++ crms/students/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Student(models.Model):
+    student_id = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
